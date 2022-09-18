2b608fe6bdbc43750f6586224f4e68fbfdf06494
kelliehale <dev.kellie.hale@gmail.com>
2022-09-18 17:19:46 -0400

    nav and profile

--- app/src/main/java/com/riding/tracker/profile/ProfileFragment.kt
+++ app/src/main/java/com/riding/tracker/profile/ProfileFragment.kt
@@ -4,16 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import com.riding.tracker.R
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var contentView: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+    ): View {
+        contentView = inflater.inflate(R.layout.profile_fragment, container, false)
+        return contentView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fullName = contentView.findViewById<TextView>(R.id.full_name)
     }
 }
