33022eb380f4a0a9f1f75899a279d8fe54b7501e
kelliehale <dev.kellie.hale@gmail.com>
2022-09-18 20:11:34 -0400

    adding app/toolbar

--- app/src/main/java/com/riding/tracker/profile/ProfileFragment.kt
+++ app/src/main/java/com/riding/tracker/profile/ProfileFragment.kt
@@ -1,9 +1,9 @@
 package com.riding.tracker.profile
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.AttributeSet
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
