24ec63cf505e09d90ec02c47fcc3f310f1c33bff
kelliehale <dev.kellie.hale@gmail.com>
2022-10-02 23:04:09 -0400

    starting guardians tab

--- app/src/main/java/com/riding/tracker/guardians/GuardiansAdapter.kt
+++ app/src/main/java/com/riding/tracker/guardians/GuardiansAdapter.kt
@@ -1,4 +1,39 @@
 package com.riding.tracker.guardians
 
-class GuardiansAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.riding.tracker.R
+
+//-- class GuardiansAdapter(val guardians: List<Guardian>)
+class GuardiansAdapter : RecyclerView.Adapter<GuardiansAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView
+        val broadcastSwitch: Switch
+
+        init {
+            nameTextView = itemView.findViewById(R.id.name_text_view)
+            broadcastSwitch = itemView.findViewById(R.id.broadcastSwitch)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+        //-- Inflate guardian_row.xml for each guardian
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+        //-- Get each guardian (val guardian = guardians[position])
+        //-- nameTextView.text = guardian.name
+        //-- broadcastSwitch.toggle = guardian.broadcast
+    }
+
+    override fun getItemCount(): Int {
+        //-- return guardians.size
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/riding/tracker/guardians/GuardiansFragment.kt
+++ app/src/main/java/com/riding/tracker/guardians/GuardiansFragment.kt
@@ -9,16 +9,32 @@ import androidx.fragment.app.Fragment
 import com.riding.tracker.R
 
 class GuardiansFragment : Fragment() {
+    private lateinit var contentView: View
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        contentView = inflater.inflate(R.layout.guardians, container, false)
+        setHasOptionsMenu(true)
+        return contentView
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.guardians)
+
+        //-- Get your RecyclerView findViewById
+        //-- Create a Guardian data class
+        //-- data class Guardian (
+        //--    private val name: String
+        //--    private val broadcast: Boolean
+        //-- )
+        //-- Create a new copy of your GuardiansAdapter
+        //-- val adapter = GuardiansAdapter
+        //-- Bind adapter
+        //-- recyclerView.adapter = guardiansAdapter
+
     }
 }
