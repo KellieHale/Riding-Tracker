707bcf8b5bd1de2ab54db51fe50f7b70ba581ec5
kelliehale <dev.kellie.hale@gmail.com>
2023-01-03 15:39:06 -0500

    got map working on Current Ride. Marker set to Lat,Lng 0.0, 0.0 . will need to update to current location

--- app/src/main/java/com/riding/tracker/currentride/CurrentRideFragment.kt
+++ app/src/main/java/com/riding/tracker/currentride/CurrentRideFragment.kt
@@ -6,20 +6,43 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.riding.tracker.R
 
-class CurrentRideFragment : Fragment() {
+class CurrentRideFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var contentView: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+    ): View {
+        contentView = inflater.inflate(R.layout.maplayout, container, false)
+        return contentView
+
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.current_ride)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
     }
 }
