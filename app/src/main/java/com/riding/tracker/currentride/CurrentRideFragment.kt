bcdde3366878213aa3950c0a0d121c03d0eacb39
kelliehale <dev.kellie.hale@gmail.com>
2023-04-13 17:07:05 -0400

    working on device location and moving items in current ride fragment to viewmodel

--- app/src/main/java/com/riding/tracker/currentride/CurrentRideFragment.kt
+++ app/src/main/java/com/riding/tracker/currentride/CurrentRideFragment.kt
@@ -1,26 +1,45 @@
 package com.riding.tracker.currentride
 
-import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.app.AlertDialog
+import android.Manifest
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts.*
-import androidx.appcompat.app.AppCompatActivity
+import android.view.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.gms.common.ConnectionResult
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.riding.tracker.R
 
 
 class CurrentRideFragment : Fragment(), OnMapReadyCallback {
 
+    private val viewModel: CurrentRideViewModel by lazy {
+        ViewModelProvider(this)[CurrentRideViewModel::class.java]
+    }
+    //-- Need binding possible for ViewModel--//
+
+    private lateinit var map: GoogleMap
+
     private lateinit var contentView: View
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private var longitude = 0.0
+    private var latitude = 0.0
+    private var homeLatLng = LatLng(latitude, longitude)
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,75 +47,88 @@ class CurrentRideFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
 
     ): View {
+
         contentView = inflater.inflate(R.layout.maplayout, container, false)
+        setHasOptionsMenu(true)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
         return contentView
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.current_ride)
-
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         }
 
-    override fun onMapReady(p0: GoogleMap) {
-        TODO("Not yet implemented")
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.map_options, menu)
     }
 
-    override fun onConnected(p0: Bundle?) {
-        TODO("Not yet implemented")
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        // Change the map type based on the user's selection.
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
-    override fun onConnectionSuspended(p0: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        map.addMarker(MarkerOptions().position(homeLatLng))
+        map.moveCamera(CameraUpdateFactory.newLatLng(homeLatLng))
 
-    private fun getDeviceLocation() {
+        checkForLocationPermission()
     }
-
-    private fun updateLocationUI() {
+    fun checkForLocationPermission() {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                getDeviceLocation()
+            }
+            else -> {
+                requestLocationPermissionLauncher.
+                    launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
     }
+    private fun getDeviceLocation(): (DialogInterface, Int) -> Unit {
+        checkForLocationPermission()
+        fusedLocationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object :
+            CancellationToken() {
+            override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
 
+            override fun isCancellationRequested() = false
+        })
+            .addOnSuccessListener { location ->
+                latitude = location.latitude
+                longitude =location.longitude
+            }
+        return getDeviceLocation()
+    }
     private val requestLocationPermissionLauncher =
         registerForActivityResult(
-            RequestPermission()
+            ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
                 getDeviceLocation()
             } else {
-                showLocationPermissionErrorDialog()
+                viewModel.showLocationPermissionErrorDialog(context)
             }
         }
 
-    private fun checkForLocationPermission() {
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                getDeviceLocation()
-                updateLocationUI()
-            }
-            else -> {
-                requestLocationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
-            }
-        }
-    }
-
-    private fun showLocationPermissionErrorDialog() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle(R.string.location_permission_error_title)
-        builder.setMessage(R.string.location_permission_error_message)
-//        builder.setPositiveButton(R.string.allow)
-        builder.setNegativeButton(R.string.deny, null)
-        builder.setNeutralButton(R.string.cancel, null)
-        builder.show()
-    }
-
-    override fun onConnectionFailed(p0: ConnectionResult) {
-        TODO("Not yet implemented")
-    }
 }
---
+++ app/src/main/java/com/riding/tracker/currentride/CurrentRideViewModel.kt
@@ -0,0 +1,54 @@
+package com.riding.tracker.currentride
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.location.Location
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.riding.tracker.R
+
+class CurrentRideViewModel: ViewModel() {
+
+    private val _latitude = MutableLiveData<Location>()
+    val latitude: LiveData<Location>
+        get() = _latitude
+
+    private val _longitude = MutableLiveData<Location>()
+    val longitude: LiveData<Location>
+        get() = _longitude
+
+
+
+    private fun onConnected(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun onConnectionSuspended(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun onConnectionFailed(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun updateLocationUI(): (DialogInterface, Int) -> Unit {
+        onConnected(0)
+        onConnectionSuspended(0)
+        onConnectionFailed(0)
+
+        return updateLocationUI()
+    }
+
+    fun showLocationPermissionErrorDialog(context: Context?) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(R.string.location_permission_error_title)
+        builder.setMessage(R.string.location_permission_error_message)
+        builder.setPositiveButton(R.string.allow, DialogInterface.OnClickListener(updateLocationUI()))
+        builder.setNegativeButton(R.string.deny, null)
+        builder.setNeutralButton(R.string.cancel, null)
+        builder.show()
+    }
+
+}
